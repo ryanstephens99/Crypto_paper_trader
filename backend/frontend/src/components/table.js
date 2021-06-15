b13885d3d2ad4d9448c4fe5ff398ee8f5f2dcc70
Ryan Stephens <ryanstephens99@gmail.com>
2021-06-15 16:18:27 -0500

    got basic chart data to dispay

--- backend/frontend/src/components/table.js
+++ backend/frontend/src/components/table.js
@@ -1,3 +1,55 @@
-// import {Line} from 'react-chartjs-2';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import {Line} from 'react-chartjs-2';
 
-// function lineChart()
+
+const DynamicChart = () => {
+    const [chartData, setChartData] = useState({});
+
+    const Chart = () => {
+        let dates = [];
+        let prices = [];
+        axios.get("http://localhost:8000/api/market")
+        .then(dat => {
+            console.log(dat.data);
+            for(const obj of dat.data){
+                prices.push(parseFloat(obj.price));
+                dates.push(Date.parse(obj.timestamp));
+            }
+            setChartData({
+                labels: dates,
+                datasets: [{
+                    label: 'price',
+                    data: prices,
+                }]
+            });
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    }
+    useEffect(() => {
+        Chart();
+    }, []);
+    return (
+        <div className="table">
+            <h1>Line Chart</h1>
+            <Line
+                data={chartData}
+                options={{
+                    responsive: true,
+                    title: { text: "we hoping out here", display: true },
+                    scales: {
+                        yAxes: {
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+export default DynamicChart;
